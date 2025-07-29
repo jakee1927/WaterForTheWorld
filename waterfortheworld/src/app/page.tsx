6972ab00188c21305e752b55548e7f55967a8fcf
jakee1927 <jakee8228@gmail.com>
2025-07-29 15:59:19 -0600

    removed unused imports

--- waterfortheworld/src/app/page.tsx
+++ waterfortheworld/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
-import { Droplets, BookOpen, Heart, Instagram, Droplet, BrainCircuit, Bot } from "lucide-react"
+import { Droplets, Heart, Instagram, BrainCircuit } from "lucide-react"
 import Link from "next/link"
 
 export default function HomePage() {
